a43d0333f3b7fef23dbada2b6d6cebdf518d4457
Kenichi Kamiya <kachick1@gmail.com>
2012-09-14 20:23:05 +0900

    doc

---
+++ example/README.rb
@@ -0,0 +1,25 @@
+$VERBOSE = true
+
+require_relative '../lib/module/mixy'
+
+module Foo
+  def common
+    'FOO :)'
+  end
+end
+
+module Bar
+  def common
+    'bar :('
+  end
+end
+
+class FooBar
+  mixy Foo, :common => :foo_common
+  mixy Bar, :common => :bar_common
+end
+
+foobar = FooBar.new
+p foobar.methods.grep(/common/) #=> [:bar_common, :foo_common]
+p foobar.foo_common             #=> "FOO :)"
+p foobar.bar_common             #=> "bar :("
